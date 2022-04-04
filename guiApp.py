94b83287f8c2a08c9e3edfdc8c3c59224b9e04ca
Parker Morrison <parkermorrison@live.ca>
2022-04-03 23:51:43 -0300

    featured results, other minor changes

--- guiApp.py
+++ guiApp.py
@@ -12,37 +12,29 @@ root.resizable(False,False)
 
 style = ttk.Style()
 style.configure('TNotebook.Tab',font=('Circular Std Medium',10)) # since the tabs are not a different widget type, we can set up a style for them
+style.configure('Special.TButton',background='#bead96')
 style.configure('Special.TFrame',background='#bead96')
+style.configure('Special.TLabel',background='#bead96')
 
-frame1 = ttk.Frame(root)
-frame2 = ttk.Frame(root)
+frame1 = ttk.Frame(root,relief=FLAT)
+frame2 = ttk.Frame(root,relief=FLAT)
 frame1.grid(row=0,column=0)
 frame2.grid(row=0,column=1)
 
-# when we actually add our elements, we'll have to use either grid or place down below.
-# pack absolutely tosses these dimensions out the window
-frame1.config(relief=FLAT)
-frame2.config(relief=FLAT)
-
-label_title = ttk.Label(frame1,text="Desired Job Title")
-label_city = ttk.Label(frame1,text="City")
-label_prov = ttk.Label(frame1,text="Province")
-label_key = ttk.Label(frame1,text="Keywords")
-
-label_title.config(font=('Circular Std Medium',18,'bold')) # font name, size, and style
-label_city.config(font=('Circular Std Medium',18,'bold'))
-label_prov.config(font=('Circular Std Medium',18,'bold'))
-label_key.config(font=('Circular Std Medium',18,'bold'))
+label_title = ttk.Label(frame1,text="Desired Job Title",font=('Circular Std Medium',18,'bold'))
+label_city = ttk.Label(frame1,text="City",font=('Circular Std Medium',18,'bold'))
+label_prov = ttk.Label(frame1,text="Province",font=('Circular Std Medium',18,'bold'))
+label_key = ttk.Label(frame1,text="Keywords",font=('Circular Std Medium',18,'bold'))
 
 entry_title = ttk.Entry(frame1,font=('Circular Std Medium',10),width=30)
 entry_city = ttk.Entry(frame1,font=('Circular Std Medium',10),width=30)
 entry_key = ttk.Entry(frame1,font=('Circular Std Medium',10),width=30)
 
 province = StringVar()
-combobox_prov = ttk.Combobox(frame1,font=('Circular Std Medium',10),textvariable=province,width=28) # 2 less since the arrow icon takes up added space
-combobox_prov.config(values=('Alberta','British Columbia','Manitoba','New Brunswick','Newfoundland and Labrador',
+combobox_prov = ttk.Combobox(frame1,font=('Circular Std Medium',10),textvariable=province,width=28,
+                             values=('Alberta','British Columbia','Manitoba','New Brunswick','Newfoundland and Labrador',
                                    'Northwest Territories','Nova Scotia','Nunavut','Ontario','Prince Edward Island',
-                                   'Quebec','Saskatchewan','Yukon'))
+                                   'Quebec','Saskatchewan','Yukon')) # 2 less since the arrow icon takes up added space
 combobox_prov.state(['readonly']) # prevents user from typing in combobox
 province.set('Alberta')
 
@@ -57,6 +49,9 @@ small_logo = Image.open('C:/Users/Owner/Desktop/python-icom3010/cb_small.png')
 small_logo = small_logo.resize((60,60),Image.ANTIALIAS)
 small_logo_pi = ImageTk.PhotoImage(small_logo)
 
+feat = BooleanVar()
+feat_check = ttk.Checkbutton(root,text="Include featured results (may be irrelevant)",
+                             variable=feat,onvalue=True,offvalue=False)
 btn_submit = ttk.Button(frame1,text="Submit",command=lambda:scrape())
 
 logo_label.grid(row=0,column=0)
@@ -70,30 +65,11 @@ combobox_prov.grid(row=6,column=0)
 label_key.grid(row=7,column=0,padx=(10,10),sticky='w')
 entry_key.grid(row=8,column=0)
 
-btn_submit.grid(row=9,column=0,padx=(10,10),pady=(10,10),sticky='e')
+feat_check.grid(row=9,column=0)
+btn_submit.grid(row=10,column=0,padx=(10,10),pady=(10,10),sticky='e')
 
 job_notebook = ttk.Notebook(frame2,height=480,width=376)
 job_notebook.grid(row=0,column=0)
-
-# initialization function, which we call to set up each notebook tab
-# we give the canvas its scrollbar, setup the scrollbar's dimensions and canvas' position,
-# then the scrollbar's position, and ensure the canvas is actually created with the frame we specified.
-def tab_init(canvas,frame,scrollbar):
-    canvas.config(yscrollcommand=scrollbar.set)
-    scrollbar.config(orient=VERTICAL,command=canvas.yview)
-    canvas.grid(row=1,column=0)
-    scrollbar.grid(row=1,column=1,sticky='ns')
-    canvas.create_window(0,0,window=frame)
-    
-
-# update function, which we will call upon inserting a new entry from our searches / queries
-# update_idletasks() lets us process any pending events WITHOUT inadvertently triggering callbacks
-# and we update the canvas' scrollregion to fit the new contents (bbox() gets its dimensions)
-def tab_upd(canvas):
-    canvas.update_idletasks()
-    canvas.config(scrollregion=canvas.bbox("all"))
-    canvas.yview_moveto(0.0)
-
 
 # each tab needs its own frame
 # each of these frames has a canvas that allows for scrollbar attachment, and which contains a frame that houses the items we'll be storing within it
@@ -118,6 +94,26 @@ feat_frame = ttk.Frame(feat_canvas)
 feat_frame.pack()
 feat_scrollbar = ttk.Scrollbar(tab_feat)
 
+# initialization function, which we call to set up each notebook tab
+# we give the canvas its scrollbar, setup the scrollbar's dimensions and canvas' position,
+# then the scrollbar's position, and ensure the canvas is actually created with the frame we specified.
+def tab_init(canvas,frame,scrollbar):
+    canvas.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(orient=VERTICAL,command=canvas.yview)
+    canvas.grid(row=1,column=0)
+    scrollbar.grid(row=1,column=1,sticky='ns')
+    canvas.create_window(0,0,window=frame)
+    
+
+# update function, which we will call upon inserting a new entry from our searches / queries
+# update_idletasks() lets us process any pending events WITHOUT inadvertently triggering callbacks
+# and we update the canvas' scrollregion to fit the new contents (bbox() gets its dimensions)
+def tab_upd(canvas):
+    canvas.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+    canvas.yview_moveto(0.0)
+    
+
 tab_init(fav_canvas,fav_frame,fav_scrollbar)
 tab_init(res_canvas,res_frame,res_scrollbar)
 tab_init(feat_canvas,feat_frame,feat_scrollbar)
@@ -130,7 +126,7 @@ job_notebook.select(0)
 def scrape():
     job_notebook.select(1)
     print(entry_key.get().split(','))
-    arr = mainScrape.basic_scrape(entry_title.get(),entry_city.get(),combobox_prov.get(),entry_key.get().split(','))
+    arr = mainScrape.basic_scrape(entry_title.get(),entry_city.get(),combobox_prov.get(),entry_key.get().split(','),feat.get())
     cnt = 0
     
     if not isinstance(arr,list):
@@ -140,32 +136,33 @@ def scrape():
         widget.destroy()
     
     for listing in arr:
-        listing_frame = ttk.Frame(res_frame)
+        if not listing.featured:
+            listing_frame = ttk.Frame(res_frame)
+        else:
+            listing_frame = ttk.Frame(feat_frame)
         listing_frame.grid_propagate(0) # keeps our frames a consistent size
         listing_frame.config(width=360,height=128,
                              relief=RIDGE,padding=(5,5))
-        
-        if listing.keyworded:
-            listing_frame.config(style='Special.TFrame')
         
         icon_label = ttk.Label(listing_frame,image=small_logo_pi)
         #icon_label.config(background=listing_frame['background'])
         icon_label.image = small_logo_pi
         
-        listing_title = Message(listing_frame,text=listing.title)
-        listing_title.config(font=('Circular Std Medium',10,'bold'),width=240)
+        listing_title = Message(listing_frame,text=listing.title,font=('Circular Std Medium',10,'bold'),width=240)
+        listing_company = Message(listing_frame,text=listing.company,font=('Circular Std Medium',10,'normal'),width=180)
+        listing_location = ttk.Label(listing_frame,text=listing.location,font=('Circular Std Medium',10,'normal'))
+        listing_date = ttk.Label(listing_frame,text=f"Posted on {listing.date}",font=('Circular Std Medium',10,'normal'))
         
-        listing_company = Message(listing_frame,text=listing.company)
-        listing_company.config(font=('Circular Std Medium',10,'normal'),width=180)
+        listing_button = ttk.Button(listing_frame,width=10,text="Apply",command=lambda listing=listing: webbrowser.open(listing.link)) # stores the listing properties; otherwise this points to the very last link element
         
-        listing_location = ttk.Label(listing_frame,text=listing.location)
-        listing_location.config(font=('Circular Std Medium',10,'normal'))
-        
-        listing_date = ttk.Label(listing_frame,text=f"Posted on {listing.date}")
-        listing_date.config(font=('Circular Std Medium',10,'normal'))
-        
-        listing_button = ttk.Button(listing_frame)
-        listing_button.config(width=10,text="Apply",command=lambda listing=listing: webbrowser.open(listing.link)) # stores the listing properties; otherwise this points to the very last link element
+        if listing.keyworded:
+            listing_frame.config(style='Special.TFrame')
+            icon_label.config(background='#bead96')
+            listing_title.config(background='#bead96')
+            listing_company.config(background='#bead96')
+            listing_location.config(style='Special.TLabel')
+            listing_date.config(style='Special.TLabel')
+            listing_button.config(style='Special.TButton')
         
         icon_label.grid(row=0,column=0,rowspan=2)
         listing_title.grid(row=0,column=1)
@@ -178,6 +175,7 @@ def scrape():
         
         listing_frame.grid(row=cnt,column=1)
         tab_upd(res_canvas)
+        tab_upd(feat_canvas)
         cnt += 1
     
 root.mainloop()
--- mainScrape.py
+++ mainScrape.py
@@ -4,13 +4,14 @@ import requests
 import cfgScrape as cfg
 
 class Listing:
-    def __init__(self,title,company,location,date,link,keyworded):
+    def __init__(self,title,company,location,date,link,keyworded,featured):
         self.title = title
         self.company = company
         self.location = location
         self.date = date
         self.link = link
         self.keyworded = keyworded
+        self.featured = featured
 
 
 """
@@ -30,11 +31,12 @@ searchKey = input("Enter a keyword you'd like to filter by: ")
 print(searchKey)
 """
 
-def basic_scrape(title,city,prov,keywords):
+def basic_scrape(title,city,prov,keywords,feat):
     pNum = 1
     maxPNum = 1
     
     arr_listing = []
+    arr_feat = []
     
     if not city:
         prov += '-jobs'
@@ -78,20 +80,21 @@ def basic_scrape(title,city,prov,keywords):
             job_date = (job.find("div", class_="job_pub_date")).attrs['title']
             job_link = (job.find("a")).attrs['href']
             
-            arr_listing.append(Listing(job_title.text.strip(),job_company.text.strip(),job_location.text.strip(),job_date,job_link,False))
+            arr_listing.append(Listing(job_title.text.strip(),job_company.text.strip(),job_location.text.strip(),job_date,job_link,False,False))
 
-            
+            """
             print(job_title.text.strip()) # 'strip' allows us to cut out any interfering tags (but not their contents)
             print(job_company.text.strip())
             print(job_location.text.strip())
             print(job_date) # raw date is actually contained within the 'title' attribute, and here's how we extract it
             print(job_link)
             print('\n'*2)
+            """
             
         pNum += 1
     
     for listing in arr_listing:
-        if keywords is None:
+        if keywords is None or '' in keywords and len(keywords) == 1:
             break
         
         keyURL = listing.link
@@ -111,85 +114,20 @@ def basic_scrape(title,city,prov,keywords):
             
     arr_listing.sort(key=lambda listing: listing.keyworded,reverse=True) # sorts the list in descending order based on if keywords were marked as present (True > False)
     
+    # featured jobs return irrelevant results; fetch them after the important listings and mark them to be placed in a separate tab
+    if feat is True:
+        feat_jobs = results.find_all("div", class_="featured_job_content")
+        for feat_job in feat_jobs:
+            feat_job_title = feat_job.find("div", class_="job_title")
+            feat_job_company = feat_job.find("span", class_="name")
+            feat_job_location = feat_job.find("span", class_="location") # this can be blank!
+            feat_job_date = (feat_job.find("div", class_="job_pub_date")).attrs['title']
+            feat_job_link = (feat_job.find("a")).attrs['href']
+            
+            arr_feat.append(Listing(feat_job_title.text.strip(),feat_job_company.text.strip(),feat_job_location.text.strip(),feat_job_date,feat_job_link,False,True))
+    
+        arr_listing = arr_listing + arr_feat
+    
     return arr_listing
 
-        
-                
-    
-"""   
-pNum = 1
-maxPNum = 1
-while pNum <= maxPNum:
-    pNumUL = soup.find('ul', class_="pagination")
-    pNumLI = pNumUL.find_all('li')
-    for li in pNumLI:
-        try:
-            maxPNum = int(li.text.strip())
-        except ValueError:
-            pass
-        
-        
-        if li.has_attr('aria-label') and li['aria-label'] == "Last":
-            break
-    
-    pNum += 1
-    
-    URL = f"https://www.careerbeacon.com/en/search/{searchFor}-jobs-in-{searchCity}_{searchProv}?page={pNum}"
-    page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    # removes the hidden 'in' that is for some reason on the CareerBeacon page
-    for span in soup.find_all("span", class_="lower hidden-xs"): 
-        span.decompose()
-
-    results = soup.find(id="search_result")
-    jobs = results.find_all("div", class_="non_featured_job_content")
-
-    for job in jobs:
-        job_title = job.find("div", class_="job_title")
-        job_company = job.find("span", class_="name")
-        job_location = job.find("span", class_="location") # this can be blank!
-        job_date_input = job.find("div", class_="job_pub_date")
-        job_date = job_date_input.attrs['title']
-    
-        print(job_title.text.strip()) # 'strip' allows us to cut out any interfering tags (but not their contents)
-        print(job_company.text.strip())
-        print(job_location.text.strip())
-        print(job_date) # raw date is actually contained within the 'title' attribute, and here's how we extract it
-        print('\n'*2)
-
-    # we can also do this but it returns irrelevant results? so probably avoid it
-    if pNum == maxPNum:
-        featured_jobs = results.find_all("div", class_="featured_job_content")
-
-        for featured_job in featured_jobs:
-            featured_title = featured_job.find("div", class_="job_title")
-            featured_company = featured_job.find("span", class_="name")
-            featured_location = featured_job.find("div", class_="job_location mid-grey")
-            featured_date_input = featured_job.find("div", class_="job_pub_date")
-            featured_date = featured_date_input.attrs['title']
-    
-            print(featured_title.text.strip())
-            print(featured_company.text.strip())
-            print(featured_location.text.strip())
-            print(featured_date)
-            print('\n'*2)
-"""
-    
-    
-    
-"""
-key_jobs = results.find_all(
-    "div", string=lambda text: f"{searchKey}" in text.lower()
-)
-key_job_elements = [
-    div_element.parent.parent.parent for div_element in key_jobs
-]
-for job_element in key_job_elements:
-    # -- snip --
-    link_url = job_element.find_all("a")["href"]
-    print(f"Apply here: {link_url}\n")
-        
-print(len(key_jobs))
-"""
